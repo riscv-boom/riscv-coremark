eb020684ef5d5cfc8a590afb75ac26a8d87b7046
Jerry Zhao <jerryz123@berkeley.edu>
2019-11-13 13:31:11 -0800

    Add baremetal Coremark build

--- build-coremark.sh
+++ build-coremark.sh
@@ -10,5 +10,7 @@ cd $BASEDIR/$CM_FOLDER
 # run the compile
 echo "Start compilation"
 make PORT_DIR=../riscv64 compile
-
 mv coremark.riscv ../
+
+make PORT_DIR=../riscv64-baremetal compile
+mv coremark.riscv-bm ../
