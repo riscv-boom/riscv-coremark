c5833c3c83406a5235db1b171713e6536a99fa53
Maciej Kurc <mkurc@antmicro.com>
2024-02-05 12:29:26 +0100

    Do not force ITERATIONS for bare metal build
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- build-coremark.sh
+++ build-coremark.sh
@@ -12,5 +12,5 @@ echo "Start compilation"
 make PORT_DIR=../riscv64 compile
 mv coremark.riscv ../
 
-make PORT_DIR=../riscv64-baremetal compile ITERATIONS=100
+make PORT_DIR=../riscv64-baremetal compile
 mv coremark.bare.riscv ../
